617d25449dccf374082531d969f7f5da1914e07f
unknown <saber.yacoubi@gmail.com>
2022-01-03 16:26:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Ppicture } from "./Component/profilePhoto";
+import { Fname } from "./Component/fullname";
+import { Addr } from "./Component/adress";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ppicture></Ppicture>
+      <Fname></Fname>
+      <Addr></Addr>
     </div>
   );
 }
